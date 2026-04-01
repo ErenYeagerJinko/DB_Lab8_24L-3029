77261347f1270e48442a4eda1cd60fd806c0cf60
Eren Yeager <erenyeagerabdulahad@gmail.com>
2026-04-01 12:30:58 +0500

    SQL Procedures

---
+++ Lab-8_24L-3029.sql
@@ -0,0 +1,35 @@
+create procedure sp_LoginUser
+@Email VARCHAR(100),
+@Password VARCHAR(100)
+as
+begin
+select UserID, FullName from Users
+where Email = @Email and PasswordHash = @Password;
+end;
+go
+
+create procedure sp_GetExperience
+@UserID int
+as
+begin
+select ExpID, JobTitle, CompanyName, YearsWorked, IsCurrentJob
+from Experience
+where UserID = @UserID
+order by ExpID desc;
+end;
+go
+
+create procedure sp_AddExperience
+@UserID int,
+@JobTitle varchar(100),
+@CompanyName varchar(100),
+@YearsWorked int
+as
+begin
+insert into Experience (UserID, JobTitle, CompanyName, YearsWorked, IsCurrentJob)
+values (@UserID, @JobTitle, @CompanyName, @YearsWorked, 0);
+
+select 'Experience added successfully!' as Message,
+        scope_identity() as NewExpID;
+end;
+go
